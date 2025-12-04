194310a4ec1cde6209c68373169add980123634c
marcotristani <marco.tristani@hotmail.it>
2025-12-04 14:41:20 +0100

    flat: creata logica esercizio mail

---
+++ js-mail/js/script.js
@@ -0,0 +1,4 @@
+//Creare array con n email x
+//chiedere email a utente tramite prompt
+//verificare se tra le email della lista c'è l'email uguale a quella inserita dall'utente
+//se la consìdizione si verifica dare esito positivo altrimenti dare esito negativo
