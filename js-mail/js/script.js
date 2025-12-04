1b314226c73f0792563a33a66061f25d8d8902f1
marcotristani <marco.tristani@hotmail.it>
2025-12-04 14:48:54 +0100

    flat: generato array email di accesso e prompt per inserire email user

--- js-mail/js/script.js
+++ js-mail/js/script.js
@@ -1,4 +1,9 @@
 //Creare array con n email x
+emailAccess = ['prova.dd@hotmail.it', 'ahahhaha@hotmail.it', 'astrodelciel@gmail.com', 'vivalapappa@gmail.com','programmareinjs.stiamomessimale.com'];
+console.log(emailArray);
+
 //chiedere email a utente tramite prompt
+emailUser = prompt('Inserire mail');
+console.log(emailUser);
 //verificare se tra le email della lista c'è l'email uguale a quella inserita dall'utente
 //se la consìdizione si verifica dare esito positivo altrimenti dare esito negativo
