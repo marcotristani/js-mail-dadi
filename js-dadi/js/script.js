9ffd2cec3006929fbe78cd0b63460cd6d905d8a8
marcotristani <marco.tristani@hotmail.it>
2025-12-04 15:08:57 +0100

    flat: scritta logica js-dadi

---
+++ js-dadi/js/script.js
@@ -0,0 +1,6 @@
+//generare un numero random da pc ta 1 e 6
+//far inserire un numero all'utente con prompt
+// verificare che queto numero sia tra 1-6
+// se è cosi allora confrontarlo con numero del pc
+// se maggiore il numero del pc allora l'utente ha perso, altrimenti ha vinto
+// tenere presente anche la condizione in cui pc e giocatore dicano lo steso numero, allora sarà pareggio
