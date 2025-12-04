145529cdda80177c32bc8049a1c13520692fefc6
marcotristani <marco.tristani@hotmail.it>
2025-12-04 14:56:34 +0100

    flat: verifica condizione di ugualianza emailUser con un email tra le email accessibili dell'array emailAccess

--- js-mail/js/script.js
+++ js-mail/js/script.js
@@ -1,9 +1,19 @@
 //Creare array con n email x
 emailAccess = ['prova.dd@hotmail.it', 'ahahhaha@hotmail.it', 'astrodelciel@gmail.com', 'vivalapappa@gmail.com','programmareinjs.stiamomessimale.com'];
-console.log(emailArray);
+console.log(emailAccess);
 
 //chiedere email a utente tramite prompt
 emailUser = prompt('Inserire mail');
 console.log(emailUser);
+
 //verificare se tra le email della lista c'è l'email uguale a quella inserita dall'utente
-//se la consìdizione si verifica dare esito positivo altrimenti dare esito negativo
+let messaggio = "Accesso negato";//Esito negativo di default
+
+for(let i = 0; i < emailAccess.lenght || emailUser === emailAccess[i]; i++ ){
+    if ( emailUser === emailAccess[i]){
+        messaggio = "Accesso approvato";//se la consìdizione si verifica dare esito positivo altrimenti dare esito negativo(di default)
+    }
+}
+
+//OUTPUT
+console.log(messaggio);
